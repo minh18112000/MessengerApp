176947ad9706b555b3111bba33449f81a581de0c
minh18112000 <nguyenducminh_t63@hus.edu.vn>
2021-10-14 15:09:58 +0700

    Setup adapter for UserRecyclerView

--- app/src/main/java/com/example/messengerapp/MainActivity.kt
+++ app/src/main/java/com/example/messengerapp/MainActivity.kt
@@ -2,10 +2,24 @@ package com.example.messengerapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var userRecyclerView: RecyclerView
+    private lateinit var userAdapter: UserAdapter
+    private lateinit var userList: ArrayList<User>
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        userList = ArrayList()
+        userAdapter = UserAdapter(this, userList)
+        userRecyclerView = findViewById(R.id.userRecyclerView)
+
+        userRecyclerView.layoutManager = LinearLayoutManager(this)
+        userRecyclerView.adapter = userAdapter
     }
 }
