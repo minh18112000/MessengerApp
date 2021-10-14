781ee2938a089ab97e8bbf2695a6d1102aaa80a2
minh18112000 <nguyenducminh_t63@hus.edu.vn>
2021-10-14 11:35:46 +0700

    Use firebaseAuth for logIn and signIn

--- app/src/main/java/com/example/messengerapp/LogIn.kt
+++ app/src/main/java/com/example/messengerapp/LogIn.kt
@@ -1,11 +1,67 @@
 package com.example.messengerapp
 
+import android.content.ContentValues
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.ktx.Firebase
 
 class LogIn : AppCompatActivity() {
+
+    private lateinit var edtEmail: EditText
+    private lateinit var edtPassword: EditText
+    private lateinit var btnLogin: Button
+    private lateinit var btnSignUp: Button
+
+    private lateinit var mAuth: FirebaseAuth
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_log_in)
+
+        mAuth = FirebaseAuth.getInstance()
+
+        edtEmail = findViewById(R.id.edt_email)
+        edtPassword = findViewById(R.id.edt_password)
+        btnLogin = findViewById(R.id.btnLogin)
+        btnSignUp = findViewById(R.id.btnSignUp)
+
+
+        btnSignUp.setOnClickListener {
+            val intent = Intent(this, SignUp::class.java)
+            startActivity(intent)
+        }
+        btnLogin.setOnClickListener {
+            val email = edtEmail.text.toString()
+            val password = edtPassword.text.toString()
+            login(email, password)
+        }
+    }
+
+    private fun login(email: String, password: String) {
+        // logic for logging user
+        mAuth.signInWithEmailAndPassword(email, password)
+            .addOnCompleteListener(this) { task ->
+                if (task.isSuccessful) {
+                    // Sign in success, update UI with the signed-in user's information
+                    // code for logging in user
+                    val intent = Intent(this, MainActivity::class.java)
+                    finish()
+                    startActivity(intent)
+                } else {
+                    // If sign in fails, display a message to the user.
+                    Log.w(ContentValues.TAG, "signInWithEmail:failure", task.exception)
+                    Toast.makeText(
+                        this,
+                        "${task.exception}",
+                        Toast.LENGTH_SHORT
+                    ).show()
+                }
+            }
     }
 }
--- app/src/main/java/com/example/messengerapp/SignUp.kt
+++ app/src/main/java/com/example/messengerapp/SignUp.kt
@@ -1,11 +1,68 @@
 package com.example.messengerapp
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.ContentValues
+import android.content.Intent
 import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.auth.FirebaseAuth
 
 class SignUp : AppCompatActivity() {
+
+    private lateinit var edtName: EditText
+    private lateinit var edtEmail: EditText
+    private lateinit var edtPassword: EditText
+    private lateinit var btnSignUp: Button
+
+    private lateinit var mAuth: FirebaseAuth
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sign_up)
+
+        mAuth = FirebaseAuth.getInstance()
+
+        edtName = findViewById(R.id.edt_name)
+        edtEmail = findViewById(R.id.edt_email)
+        edtPassword = findViewById(R.id.edt_password)
+        btnSignUp = findViewById(R.id.btnSignUp)
+
+        btnSignUp.setOnClickListener {
+            val name = edtName.text.toString()
+            val email = edtEmail.text.toString()
+            val password = edtPassword.text.toString()
+            signUp(name, email, password)
+        }
+    }
+
+    private fun signUp(name: String, email: String, password: String) {
+        // logic of creating user
+        mAuth.createUserWithEmailAndPassword(email, password)
+            .addOnCompleteListener(this) { task ->
+                if (task.isSuccessful) {
+                    // Sign in success, update UI with the signed-in user's information
+                    // code for jumping to home
+                    val intent = Intent(this, MainActivity::class.java)
+                    finish()
+                    startActivity(intent)
+
+                    // add user
+                    addUserToDatabase(name, email, mAuth.currentUser?.uid!!)
+                } else {
+                    // If sign in fails, display a message to the user.
+                    Log.w(ContentValues.TAG, "createUserWithEmail:failure", task.exception)
+                    Toast.makeText(
+                        this,
+                        "${task.exception}",
+                        Toast.LENGTH_SHORT
+                    ).show()
+                }
+            }
+    }
+
+    private fun addUserToDatabase(name: String, email: String, uid: String) {
     }
 }
